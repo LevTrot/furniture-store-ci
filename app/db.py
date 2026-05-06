0eb20a46caa8af5090bdb878302b290e8f838bc0
LevTrot <168160548+LevTrot@users.noreply.github.com>
2026-05-06 21:00:40 +0400

    Update db.py

--- app/db.py
+++ app/db.py
@@ -7,7 +7,15 @@ DATABASE_URL = os.getenv(
     "sqlite:///./test.db"
 )
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
