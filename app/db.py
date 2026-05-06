8553d911adbfca6b0b19c7516b75d66c49cea054
LevTrot <168160548+LevTrot@users.noreply.github.com>
2026-05-06 20:29:28 +0400

    Update db.py

--- app/db.py
+++ app/db.py
@@ -4,7 +4,7 @@ import os
 
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
-    "postgresql://postgres:postgres@db:5432/furniture"
+    "sqlite:///./test.db"
 )
 
 engine = create_engine(DATABASE_URL)
