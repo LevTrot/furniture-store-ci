abb603ef9d048ac6ab20106aad46b5d6b032b602
LevTrotsky <aspirine220smouk@mail.ru>
2026-05-07 15:28:23 +0400

    coverage will fail

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,6 @@ import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import sys
 
 from app.main import app
 from app.db import Base
